81ec60a05433c84c687e5cc01534a8ffc4f7429b
khodchenko <khodch3nk0@gmail.com>
2022-08-30 17:06:46 +0200

    Create Rest Homework

---
+++ Rest/LoginForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Rest
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelBackground = new System.Windows.Forms.Panel();
+            this.labelServerStatus = new System.Windows.Forms.Label();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.pictureBoxLocker = new System.Windows.Forms.PictureBox();
+            this.pictureBoxUser = new System.Windows.Forms.PictureBox();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.panelBackground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLocker)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelBackground
+            // 
+            this.panelBackground.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panelBackground.Controls.Add(this.labelServerStatus);
+            this.panelBackground.Controls.Add(this.buttonLogin);
+            this.panelBackground.Controls.Add(this.textBoxPassword);
+            this.panelBackground.Controls.Add(this.pictureBoxLocker);
+            this.panelBackground.Controls.Add(this.pictureBoxUser);
+            this.panelBackground.Controls.Add(this.textBoxLogin);
+            this.panelBackground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelBackground.Location = new System.Drawing.Point(0, 0);
+            this.panelBackground.Name = "panelBackground";
+            this.panelBackground.Size = new System.Drawing.Size(358, 387);
+            this.panelBackground.TabIndex = 0;
+            // 
+            // labelServerStatus
+            // 
+            this.labelServerStatus.AutoSize = true;
+            this.labelServerStatus.ForeColor = System.Drawing.Color.Red;
+            this.labelServerStatus.Location = new System.Drawing.Point(256, 341);
+            this.labelServerStatus.Name = "labelServerStatus";
+            this.labelServerStatus.Size = new System.Drawing.Size(90, 20);
+            this.labelServerStatus.TabIndex = 5;
+            this.labelServerStatus.Text = "server status";
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(104, 328);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(128, 47);
+            this.buttonLogin.TabIndex = 4;
+            this.buttonLogin.Text = "authorize";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.checkLogin);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxPassword.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxPassword.Location = new System.Drawing.Point(-3, 243);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PlaceholderText = "password";
+            this.textBoxPassword.Size = new System.Drawing.Size(358, 52);
+            this.textBoxPassword.TabIndex = 3;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // pictureBoxLocker
+            // 
+            this.pictureBoxLocker.Image = global::Rest.Properties.Resources.locker;
+            this.pictureBoxLocker.Location = new System.Drawing.Point(0, 164);
+            this.pictureBoxLocker.Name = "pictureBoxLocker";
+            this.pictureBoxLocker.Size = new System.Drawing.Size(358, 64);
+            this.pictureBoxLocker.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLocker.TabIndex = 2;
+            this.pictureBoxLocker.TabStop = false;
+            // 
+            // pictureBoxUser
+            // 
+            this.pictureBoxUser.Image = global::Rest.Properties.Resources.user;
+            this.pictureBoxUser.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxUser.Name = "pictureBoxUser";
+            this.pictureBoxUser.Size = new System.Drawing.Size(358, 64);
+            this.pictureBoxUser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxUser.TabIndex = 1;
+            this.pictureBoxUser.TabStop = false;
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxLogin.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxLogin.Location = new System.Drawing.Point(0, 77);
+            this.textBoxLogin.Margin = new System.Windows.Forms.Padding(3, 30, 3, 3);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.PlaceholderText = "login";
+            this.textBoxLogin.Size = new System.Drawing.Size(358, 52);
+            this.textBoxLogin.TabIndex = 0;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 387);
+            this.Controls.Add(this.panelBackground);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "LoginForm";
+            this.Text = "MOCK";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.panelBackground.ResumeLayout(false);
+            this.panelBackground.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLocker)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUser)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelBackground;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.PictureBox pictureBoxUser;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.PictureBox pictureBoxLocker;
+        private System.Windows.Forms.Label labelServerStatus;
+    }
+}
---
+++ Rest/LoginForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rest
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+
+            this.textBoxPassword.AutoSize = false;
+            this.textBoxPassword.Size = new Size(this.textBoxPassword.Size.Width, this.textBoxLogin.Height);
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void checkLogin(object sender, EventArgs e)
+        {
+          
+            this.Close();
+        }
+    }
+}
